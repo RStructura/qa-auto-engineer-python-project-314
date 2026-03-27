b47df37ddbffe207a1be60baf0f10f65aa030b1b
RStructura <roso.qaengineer@gmail.com>
2026-03-27 22:58:22 +0500

    Refactoring

--- conftest.py
+++ conftest.py
@@ -18,7 +18,7 @@ def driver():
     options = Options()
 
     # Настройка виртуального окна
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
